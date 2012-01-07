9d05ac7079a5187987f28900f552e853dca7ac75
James Cotton <peabody124@gmail.com>
2012-01-07 22:16:39 +0100

    Move stimulation table to its own database

--- schemas/+stimulation/getSchema.m
+++ schemas/+stimulation/getSchema.m
@@ -2,6 +2,6 @@ function obj = getSchema
 persistent schemaObject
 if isempty(schemaObject)
     schemaObject = dj.Schema('stimulation', 'at-storage.neusc.bcm.tmc.edu', ...
-        'james_ephys', 'jcotton', 'jcotton#1');
+        'stimulation', 'jcotton', 'jcotton#1');
 end
 obj = schemaObject;
