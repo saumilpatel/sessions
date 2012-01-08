cd7e42ffbf218dd880ce9ab26f3a468a2e74595c
James Cotton <peabody124@gmail.com>
2012-01-08 10:24:23 +0100

    Move the variational clustering files from clustering to sort

--- schemas/+clustering/getSchema.m
+++
@@ -1,7 +0,0 @@
-function obj = getSchema
-persistent schemaObject
-if isempty(schemaObject)
-    schemaObject = dj.Schema('clustering', 'at-storage.neusc.bcm.tmc.edu', ...
-        'james_ephys', 'jcotton', 'jcotton#1');
-end
-obj = schemaObject;
