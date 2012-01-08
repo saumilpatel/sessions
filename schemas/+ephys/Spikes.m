6717ed882941b4979b1e5c40893399fc8586f86f
James Cotton <peabody124@gmail.com>
2012-01-08 10:19:21 +0100

    Rename the fields in the Spikes object

--- schemas/+ephys/Spikes.m
+++ schemas/+ephys/Spikes.m
@@ -2,13 +2,12 @@
 ephys.Spikes (imported) # A spike train from a single or multi unit
 
 -> ephys.SpikeSet
-spike_id        : int unsigned          # The spike data
+unit_id        : int unsigned          # The spike data
 ---
 electrode_num=0             : int unsigned                  # The electrode number
 spike_times=null            : longblob                      # The spike timing data
-spike_waveforms=null        : longblob                      # The spike waveform data
+mean_waveform=null        : longblob                      # The spike waveform data
 spike_file_path=""          : varchar(255)                  # The file containing the spike data
-spikes_ts=CURRENT_TIMESTAMP : timestamp                     # automatic timestamp. Do not edit
 %}
 
 classdef Spikes < dj.Relvar
@@ -34,10 +33,10 @@ classdef Spikes < dj.Relvar
             disp(sprintf('Found %d spike files to import\n', length(keys)));
             for i = 1:length(keys)
                 tuple = key;
-                tuple.spike_id = i;
-                [tuple.spike_times, tuple.spike_waveforms, tuple.spike_file_path] = getSpikes(accessor & keys(i));
+                tuple.unit_id = i;
+                [tuple.spike_times, tuple.mean_waveform, tuple.spike_file_path] = getSpikes(accessor & keys(i));
                 tuple.electrode_num = keys(i).electrode_num;
-                
+                insert(ephys.Spikes, tuple)
                 % TODO: Add switch here which adds additional information in a method specific manner
             end
         end
