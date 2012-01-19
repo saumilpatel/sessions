c11fbf637a5e347985d3e380850e639aec3bd54c
James Cotton <peabody124@gmail.com>
2012-01-19 10:51:41 -0600

    Fix another indentation bug (else if instead of elseif) and also the parents of sort.Params/sort.Sets

--- schemas/+ephys/Spikes.m
+++ schemas/+ephys/Spikes.m
@@ -25,33 +25,32 @@ classdef Spikes < dj.Relvar
             
             if strcmp(type,'MultiUnit')
                 accessor = sort.MultiUnit;
-            else if strcmp(type,'VariationalClustering') || strcmp(type,'Utah')
-                    accessor = sort.VariationalClusteringSU;
-                else
-                    error('"Unimplemented"');
-                end
+            elseif strcmp(type,'VariationalClustering') || strcmp(type,'Utah')
+                accessor = sort.VariationalClusteringSU;
+            else
+                error('"Unimplemented"');
+            end
+            
+            keys = fetch(accessor & key);
+            disp(sprintf('Found %d spike files to import\n', length(keys)));
+            for i = 1:length(keys)
+                tuple = key;
+                tuple.unit_id = i;
+                [tuple.spike_times, tuple.mean_waveform, tuple.spike_file_path] = getSpikes(accessor & keys(i));
+                tuple.electrode_num = keys(i).electrode_num;
+                insert(ephys.Spikes, tuple)
                 
-                keys = fetch(accessor & key);
-                disp(sprintf('Found %d spike files to import\n', length(keys)));
-                for i = 1:length(keys)
-                    tuple = key;
-                    tuple.unit_id = i;
-                    [tuple.spike_times, tuple.mean_waveform, tuple.spike_file_path] = getSpikes(accessor & keys(i));
-                    tuple.electrode_num = keys(i).electrode_num;
-                    insert(ephys.Spikes, tuple)
+                % Adds additional information in a method specific manner
+                if strcmp(type,'VariationalClustering') || strcmp(type,'Utah')
+                    tuple_link = dj.util.structJoin(tuple, keys(i));
+                    insert(sort.VariationalClusteringLink, tuple_link);
                     
-                    % Adds additional information in a method specific manner
-                    if strcmp(type,'VariationalClustering') || strcmp(type,'Utah')
-                        tuple_link = dj.util.structJoin(tuple, keys(i));
-                        insert(sort.VariationalClusteringLink, tuple_link);
-                        
-                        vcsu = fetch(accessor & key, '*');
-                        tuple_su = tuple
-                        tuple_su.snr = vcsu.snr
-                        tuple_su.fp = vcsu.fp;
-                        tuple_su.fn = vcsu.fn;
-                        insert(ephys.SingleUnit, tuple_su);
-                    end
+                    vcsu = fetch(accessor & key, '*');
+                    tuple_su = tuple
+                    tuple_su.snr = vcsu.snr
+                    tuple_su.fp = vcsu.fp;
+                    tuple_su.fn = vcsu.fn;
+                    insert(ephys.SingleUnit, tuple_su);
                 end
             end
         end
