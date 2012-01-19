18c1e2383792da984837d40035b610922d74459d
Alex Ecker <aecker@cns.bcm.edu>
2012-01-19 03:10:12 -0600

    Changed synchronization table to throw error in case of failure instead of inserting it as unsycnhronized

--- schemas/+acq/StimulationSync.m
+++ schemas/+acq/StimulationSync.m
@@ -3,9 +3,10 @@ acq.StimulationSync (computed)   # synchronization
 
 ->acq.Stimulation
 ---
-sync_network        : boolean  # network synchronization was done
-sync_diode          : boolean  # synchronized to photodiode
-residual_rms = NULL : float    # residual root mean square
+sync_network        : boolean # network synchronization was done
+sync_diode          : boolean # synchronized to photodiode
+residual_rms = NULL : float   # residual root mean square
+diode_offset = NULL : float   # offset between photodiode and labview timer
 %}
 
 classdef StimulationSync < dj.Relvar & dj.AutoPopulate
@@ -22,39 +23,21 @@ classdef StimulationSync < dj.Relvar & dj.AutoPopulate
         
         function makeTuples(self, key)
             tuple = key;
-            tuple.sync_network = false;
             tuple.sync_diode = false;
-            
-            % load up the sync times
             stim = getStim(acq.Stimulation(key));
             
             % network sync
-            try
-                [stim, tuple.residual_rms] = syncNetwork(stim, key);
-                tuple.sync_network = true;
-            catch err
-                msg = 'Network synchronization failed (stim_start_time = %ld)\n\nError message:\n%s';
-                warning('StimulationSync:syncNetworkFailure', msg, key.stim_start_time, err.message)
-            end
-
+            [stim, tuple.residual_rms] = syncNetwork(stim, key);
+            tuple.sync_network = true;
+            
             % was the session recorded? -> sync to photodiode
             ephysKey = fetch(acq.EphysStimulationLink(key));
             if ~isempty(ephysKey)
-                try
-                    [stim, tuple.residual_rms] = syncEphys(stim, ephysKey); %#ok
-                    tuple.sync_diode = true;
-                catch err
-                    msg = 'Synchronization to photodiode failed (stim_start_time = %ld)\n\nError message:\n%s';
-                    warning('StimulationSync:syncDiodeFailure', msg, key.stim_start_time, err.message)
-                end
+                [stim, tuple.residual_rms, tuple.diode_offset] = syncEphys(stim, ephysKey); %#ok
+                tuple.sync_diode = true;
             end
             
-            % save synchronized file
-            if tuple.sync_network || tuple.sync_diode
-                save(getFileName(acq.Stimulation(key), 'Synched'), 'stim');
-            end
-            
-            % put stats into db
+            save(getFileName(acq.Stimulation(key), 'Synched'), 'stim');
             insert(self, tuple);
         end
     end
