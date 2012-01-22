3151fa6b3aa8820a78138533350cd3a1fbc4c766
James Cotton <peabody124@gmail.com>
2012-01-22 10:30:28 -0600

    Add helper function for processing Utah spikes

---
+++ processing/spikesUtah.m
@@ -0,0 +1,19 @@
+function electrodes = spikesUtah(sourceFile, spikesFile)
+% Spike detection callback for tetrodes
+% AE 2011-10-26
+
+% determine which tetrodes were recorded
+electrodes = 1:96
+
+% Extract the common reference
+extract_common_reference(sourceFile);
+
+matlabpool
+
+parfor i = 1:numel(electrodes)
+    fprintf('Extracting spikes from electrodes %d\n', i);
+    outFile = sprintf(strrep(spikesFile, '\', '\\'), electrodes(i));
+    detectSpikes(sourceFile,outFile,'tetrode',[],'channels',electrodes(i));
+end
+
+matlabpool close
