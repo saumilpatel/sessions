a6dc9e4c99a177b8e54a086059af16cf4e1e0c11
James Cotton <rcotton@cns.bcm.edu>
2012-01-22 13:15:10 -0600

    Rename the setPath functions as matlab doesn't like -

--- james_setPath.m
+++ james_setPath.m
@@ -20,14 +20,10 @@ addpath(fullfile(base, 'sync'));
 
 % DataJoint library is assumed to be in the same directory as the base
 % diretory
-ndx = find(base == filesep, 1, 'last');
-addpath(fullfile(base(1:ndx-1), 'datajoint/matlab'))
+addpath(getLocalPath('/lab/libraries/datajoint2'))
 
 % TEMP until updated on /lab/libraries
-run(fullfile(base(1:ndx-1), 'hdf5matlab/setPath.m'))
-
-% spike detection
-run(fullfile(base(1:ndx-1), 'detection/setPath.m'))
+run(getLocalPath('/lab/libraries/hdf5matlab/setPath'))
 
 % LFP
 addpath(getLocalPath('/lab/libraries/lfp'));
