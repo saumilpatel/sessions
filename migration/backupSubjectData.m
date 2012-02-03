6b9b2248b41ed591fc8caafd734262f3f2a5f4fe
Alexander Ecker <aecker@tuebingen.mpg.de>
2012-02-03 13:59:19 -0600

    Added function to backup all rows for a given subject

---
+++ migration/backupSubjectData.m
@@ -0,0 +1,24 @@
+function backupSubjectData(subjectId)
+
+tables = {'Sessions', 'SessionsIgnore', 'SessionsCleanup', 'Ephys', ...
+    'EphysIgnore', 'Stimulation', 'StimulationIgnore', ...
+    'EphysStimulationLink', 'StimulationSync', 'BehaviorTraces'};
+backupTables('acq', tables, subjectId);
+   
+tables = {'Mua', 'Lfp'};
+backupTables('cont', tables, subjectId);
+
+tables = {'Params', 'Sets', 'Electrodes'};
+backupTables('detect', tables, subjectId);
+
+tables = {'Params', 'Sets', 'SetsCompleted', 'Electrodes'};
+backupTables('sort', tables, subjectId);
+
+
+function backupTables(schema, tables, subjectId)
+
+for i = 1:numel(tables)
+    eval(sprintf('%s = fetch(%s.%s(''subject_id = %d''), ''*'');', ...
+        tables{i}, schema, tables{i}, subjectId));
+end
+save(schema, tables{:})
