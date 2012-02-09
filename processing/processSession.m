7be9c7f591e12a3160e0551c8889f3f7ae80b968
Alexander Ecker <aecker@tuebingen.mpg.de>
2012-02-09 10:57:33 -0600

    Made processSession also accept a folder instead of a primary key

--- processing/processSession.m
+++ processing/processSession.m
@@ -1,12 +1,19 @@
 function processSession(sessKey, detectMethod, sortMethod)
 % Process an acquisition session
 %   processSession(sessKey)
+%   processSession(folder)
 %   processSession(sessKey, detectMethod, sortMethod)
 %
 % AE 2011-11-11
 
 if nargin < 2, detectMethod = []; end
 if nargin < 3, sortMethod = []; end
+
+% if session is specified by folder, obtain the primary key
+if ischar(sessKey)
+    folder = strrep(sessKey, '\', '/');
+    sessKey = fetch(acq.Sessions(sprintf('session_path LIKE "%%%s%%"', folder(3:end))));
+end
 
 % do a cleanup before processing 
 % fixes missing stop_times and updates the t0 in the raw data files
