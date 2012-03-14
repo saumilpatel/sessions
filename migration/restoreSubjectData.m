14008000f0c45e4d29050efce0fee722b8349f37
Alexander Ecker <aecker@tuebingen.mpg.de>
2012-03-14 11:32:40 +0100

    Simplified restoreSubejctData()

--- migration/restoreSubjectData.m
+++ migration/restoreSubjectData.m
@@ -1,25 +1,17 @@
 function restoreSubjectData()
 
-tables = {'Sessions', 'SessionsIgnore', 'SessionsCleanup', 'EphysTypes', ...
-    'Ephys', 'EphysIgnore', 'Stimulation', 'StimulationIgnore', ...
-    'EphysStimulationLink', 'StimulationSync', 'BehaviorTraces'};
-restoreTables('acq', tables);
-   
-tables = {'Mua', 'Lfp'};
-restoreTables('cont', tables);
-
-tables = {'Params', 'Sets', 'Electrodes'};
-restoreTables('detect', tables);
-
-tables = {'Params', 'Sets', 'SetsCompleted', 'Electrodes'};
-restoreTables('sort', tables);
+restoreTables('acq');
+restoreTables('cont');
+restoreTables('detect');
+restoreTables('sort');
 
 
-function restoreTables(schema, tables)
+function restoreTables(schema)
 
-load(schema);
+data = load(schema);
+tables = fieldnames(data);
 for i = 1:numel(tables)
-    eval(sprintf('inserti(%s.%s, %s);', ...
+    eval(sprintf('inserti(%s.%s, data.%s);', ...
         schema, tables{i}, tables{i}));
 end
 save(schema, tables{:})
