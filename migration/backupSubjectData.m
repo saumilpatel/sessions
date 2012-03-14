3b9646336cb8d35eba54ed9660bc33c00031529d
Alex Ecker <aecker@cns.bcm.edu>
2012-03-14 10:21:06 -0500

    Added another table to backup/restore

--- migration/backupSubjectData.m
+++ migration/backupSubjectData.m
@@ -1,6 +1,6 @@
 function backupSubjectData(subjectId)
 
-tables = {'Sessions', 'SessionsIgnore', 'SessionsCleanup', 'EphysTypes', ...
+tables = {'Subjects', 'Sessions', 'SessionsIgnore', 'SessionsCleanup', 'EphysTypes', ...
     'Ephys', 'EphysIgnore', 'Stimulation', 'StimulationIgnore', ...
     'EphysStimulationLink', 'StimulationSync', 'BehaviorTraces'};
 backupTables('acq', tables, subjectId);
@@ -20,7 +20,7 @@ backupTables('sort', tables, subjectId);
 function backupTables(schema, tables, subjectId)
 
 for i = 1:numel(tables)
-    eval(sprintf('%s = fetch(%s.%s(''subject_id = %d''), ''*'');', ...
+    eval(sprintf('%s = fetch(%s.%s & struct(''subject_id'', %d), ''*'');', ...
         tables{i}, schema, tables{i}, subjectId));
 end
 save(schema, tables{:})
