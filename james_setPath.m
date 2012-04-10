fad3e8584260e9884db9e137376f138be1a0ea3e
James Cotton <rcotton@cns.bcm.edu>
2012-04-10 13:44:46 -0500

    Change where the DataJoint path is to the same directory as the sessions tree.

--- james_setPath.m
+++ james_setPath.m
@@ -2,12 +2,6 @@ function setPath
 
 warning off MATLAB:dispatcher:nameConflict
 
-if isequal(computer, 'PCWIN64')
-    addpath(getLocalPath('/lab/libraries/mym/win64'))
-else
-    addpath(getLocalPath('/lab/libraries/mym'))
-end
-
 % user specific DJ connection parameters (uses Alex' credentials)
 host = 'at-storage.neusc.bcm.tmc.edu';
 user = 'jcotton';
@@ -26,9 +20,19 @@ addpath(fullfile(base, 'processing/utils'))
 addpath(fullfile(base, 'recovery'))
 addpath(fullfile(base, 'schemas'))
 addpath(fullfile(base, 'migration'))
+
 % DataJoint library is assumed to be in the same directory as the base
 % diretory
-addpath(getLocalPath('/lab/libraries/datajoint2'))
+addpath(fullfile(base, '../DataJoint/matlab'));
+
+
+if isequal(computer, 'PCWIN64')
+    addpath(fullfile(base, '../DataJoint/mym/win64'));
+else
+    addpath(fullfile(base, '../DataJoint/mym'));
+end
+
+
 
 % TEMP until updated on /lab/libraries
 run(getLocalPath('/lab/libraries/hdf5matlab/setPath'))
