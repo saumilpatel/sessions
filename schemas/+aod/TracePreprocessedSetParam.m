98a2a0a742474178e76d2dce00a93326dfc1b57d
James Cotton <rcotton@cns.bcm.edu>
2012-06-17 13:27:05 -0500

    Some improvements to the quality metric for aods

--- schemas/+aod/TracePreprocessedSetParam.m
+++ schemas/+aod/TracePreprocessedSetParam.m
@@ -19,9 +19,9 @@ classdef TracePreprocessedSetParam < dj.Relvar
     
     methods (Static)
         function createSets(key ,method)
-            assert(count(key) > 0, 'No sets to attach to found');
-            preprocess_method_num = fetch1(aod.TracePreprocessedMethod & ...
-                ['preprocessed_method_name=' method], 'preprocessed_method_num');
+            assert(count(aod.TraceSet & key) > 0, 'No sets to attach to found');
+            preprocess_method_num = fetch1(aod.TracePreprocessedSetMethod & ...
+                ['preprocessed_method_name="' method '"'], 'preprocessed_method_num');
             ts = fetch(aod.TraceSet & key);
             for i = 1:length(ts)
                 tuple = ts(i);
