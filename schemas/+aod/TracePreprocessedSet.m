284ea2dec1122f383b170766bfbf35989712f9e4
James Cotton <rcotton@cns.bcm.edu>
2012-06-17 15:00:29 -0500

    Add plot function to trace set

--- schemas/+aod/TracePreprocessedSet.m
+++ schemas/+aod/TracePreprocessedSet.m
@@ -24,5 +24,16 @@ classdef TracePreprocessedSet < dj.Relvar & dj.AutoPopulate
             
             makeTuples( aod.TracePreprocessed, key )
         end
+        
+        function t = plot( this )
+            assert( count(aod.TracePreprocessedSet & this) == 1, 'Only one trace set can be plotted');
+            
+            t = fetch(aod.TracePreprocessed & this, '*');
+            t(1) = [];
+            traces = cat(2,t.trace);
+            traces(1,:) = [];
+            time = (1:size(traces,1)) / t(1).fs;
+            plot(time,bsxfun(@plus,traces,1:size(traces,2)))
+        end
     end
 end
