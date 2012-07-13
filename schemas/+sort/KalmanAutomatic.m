92be6c9b982eb8afb1769a30d68d8950e4414b2a
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-07-13 12:01:59 +0200

    Added MoKsm git hash to KalmanAutomatic table

--- schemas/+sort/KalmanAutomatic.m
+++ schemas/+sort/KalmanAutomatic.m
@@ -1,47 +1,45 @@
 %{
 sort.KalmanAutomatic (computed) # my newest table
+
 -> sort.Electrodes
------
-model: LONGBLOB # The fitted model
-kalmanautomatic_ts=CURRENT_TIMESTAMP: timestamp           # automatic timestamp. Do not edit
+---
+model                       : longblob                      # The fitted model
+git_hash=""                 : varchar(40)                   # git hash of MoKsm package
+kalmanautomatic_ts=CURRENT_TIMESTAMP: timestamp             # automatic timestamp. Do not edit
 %}
 
 classdef KalmanAutomatic < dj.Relvar & dj.AutoPopulate
-    
+
     properties(Constant)
         table = dj.Table('sort.KalmanAutomatic')
         popRel = sort.Electrodes * sort.Methods('sort_method_name = "MoKsm"');
     end
-    
+
     methods
         function self = KalmanAutomatic(varargin)
             self.restrict(varargin)
         end
     end
-    
+
     methods (Access=protected)
         function makeTuples( this, key )
             % Cluster spikes
             %
             % JC 2011-10-21
-            tuple = key;
             
             de_key = fetch(detect.Electrodes(key));
             
             m = MoKsmInterface(de_key);
-            if length(m.Waveforms.data) > 1
-                m = getFeatures(m,'PCA');
-            else
-                m = getFeatures(m,'PCA');
-            end
+            m = getFeatures(m,'PCA');
             m.params.Verbose = true;
             fitted = fit(m);
-            compressed = saveStructure(compress(fitted));
-            
             plot(fitted);
-            pause(1)
-            tuple.model = compressed;
-            insert(this,tuple);
+            drawnow
+            
+            tuple = key;
+            tuple.model = saveStructure(compress(fitted));
+            tuple.git_hash = gitHash('MoKsm');
+            insert(this, tuple);
             
             makeTuples(sort.KalmanTemp, key, fitted);
         end
