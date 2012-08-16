ae516898207d736e5a27fd57da2578cbc06c016f
Alex Ecker <aecker@tuebingen.mpg.de>
2012-08-16 09:12:28 -0500

    Added files to run MoKsm on cluster

--- processing/cluster/genConditionalRunFile.m
+++ processing/cluster/genConditionalRunFile.m
@@ -2,6 +2,6 @@ function genConditionalRunFile(n, condition)
 
 fid = fopen(sprintf('run%d', n), 'w');
 for i = 1:n
-    fprintf(fid, '%s\n', condition);
+    fprintf(fid, '%d, %s\n', i, condition);
 end
 fclose(fid);
