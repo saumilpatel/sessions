58996ef6e112866d496e9d34bbca047a1cf4459e
James Cotton <peabody124@gmail.com>
2012-08-22 09:58:04 -0500

    Remove outdated schema, especially since it was pointing at at-storage

--- schemas/+sessions/getSchema.m
+++
@@ -1,8 +0,0 @@
-function obj = getSchema
-persistent schemaObject
-if isempty(schemaObject)
-    load('djuser');
-    schemaObject = dj.Schema('sessions', 'at-storage.neusc.bcm.tmc.edu', ...
-        'acq', user, pass);
-end
-obj = schemaObject;
