15c67520c325c8a544487a19d3a490b6a3a0f8ef
James Cotton <peabody124@gmail.com>
2012-09-07 12:50:09 -0500

    Update more objects to dj.Automatic

--- schemas/+aod/ForwardVonMises.m
+++ schemas/+aod/ForwardVonMises.m
@@ -12,7 +12,7 @@ peak_amp2 : double   # dF/F at opposite direction
 von_base  : double   # dF/F base
 %}
 
-classdef ForwardVonMises < dj.Relvar & dj.AutoPopulate
+classdef ForwardVonMises < dj.Relvar & dj.Automatic
     properties(Constant)
         table = dj.Table('aod.ForwardVonMises');
         popRel = aod.ForwardTuning;
