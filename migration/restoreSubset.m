237b7f5e70693e246740258c62647f7d0edd3b82
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-05-07 11:07:58 -0500

    Updated restoreSubset method

--- migration/restoreSubset.m
+++ migration/restoreSubset.m
@@ -1,12 +1,18 @@
-function restoreSubset(filename)
+function restoreSubset(folder, tables)
 % Restore a backup created by backupSubset.
-%   restoreSubset(filename) reads the backup file created by backupSubset
-%   and restores it to the database.
+%   restoreSubset(filename, tables) reads the backup file created by
+%   backupSubset and restores it to the database.
 %
-% AE 2012-06-29
+% AE 2013-04-10
 
-backup = load(filename);
-for iTable = 1 : numel(backup.tables)
-    inserti(eval(backup.tables{iTable}), backup.data{iTable});
+for iTable = 1 : numel(tables)
+    table = tables{iTable};
+    fprintf('Restoring table %s... ', table)
+    ndx = find(table == '.', 1);
+    subfolder = table(1 : ndx - 1);
+    file = table(ndx + 1 : end);
+    backup = load(fullfile(folder, subfolder, file), 'tuples', 'table');
+    inserti(eval(backup.table), backup.tuples);
+    fprintf('done\n')
 end
-fprintf('Restored backup successfully.\n')
+fprintf('Restored backup successfully.\n\n')
