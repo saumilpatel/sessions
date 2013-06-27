f2651fda5f32c30e33d06972f8892b088d27d640
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-06-27 14:03:07 +0200

    Small typo in FrameDrops table

--- schemas/+acq/FrameDrops.m
+++ schemas/+acq/FrameDrops.m
@@ -3,8 +3,8 @@ acq.FrameDrops (manual)     # dropped frames (happens on acute setup only)
 
 -> acq.Stimulation
 -> acq.Ephys
-trial_num           : unsigned int  # trial number
-frame_num           : unsigned int  # frame number
+trial_num           : int unsigned  # trial number
+frame_num           : int unsigned  # frame number
 ---
 shift               : tinyint       # number of frames shifted
 %}
