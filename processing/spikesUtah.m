84e04a3db8e64283b12cc76f3be2afd02b367c0a
James Cotton <rcotton@cns.bcm.edu>
2014-10-08 16:33:57 -0500

    Add notes and comments

--- processing/spikesUtah.m
+++ processing/spikesUtah.m
@@ -1,22 +1,24 @@
 function [electrodes, artifacts] = spikesUtah(sourceFile, spikesFile)
-% Spike detection callback for tetrodes
-% AE 2011-10-26
-% 
-% Updated to reflect Utah array specific detect method invocation used on 
-% at-detect
-% EW 2014-06-02
+% Spike detection callback for Utah array
+% Created by James Cotton on unknown timing
+%
+% Source file is the raw file specifying pattern e.g.
+% 'Electrophysiology%d.h5'
+%
+% spikesFile - path/naming_pattern designating where detected spikes will
+% be saved to
 
 % determine which tetrodes were recorded
 electrodes = 1:96;
 
-% Extract the common reference
+% Extract the common reference - mean across channels at each timepoint
 extract_common_reference(sourceFile);
 
 matlabpool
 
 parfor i = 1:numel(electrodes)
     fprintf('Extracting spikes from electrodes %d\n', electrodes(i));
-    outFile = sprintf(strrep(spikesFile, '\', '\\'), electrodes(i));
+    outFile = sprintf(strrep(spikesFile, '\', '\\'), electrodes(i)); % probably only works on Windows
     % call Utah array specific detection routine
     artifacts{i} = detectSpikesUtah(sourceFile, electrodes(i), outFile);
 end
