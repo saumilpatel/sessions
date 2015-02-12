63b4ba74ddb63d89c5d17e656c244bfcbc692404
George Denfield <george.denfield@gmail.com>
2015-02-12 13:39:00 -0600

    created file for new SP detection method that calls extract_common_referenceSP followed by detectSpikesSPV2 to handle common reference subtraction with detection

---
+++ processing/spikesSiliconProbesV2.m
@@ -0,0 +1,20 @@
+function [channels, artifacts] = spikesSiliconProbesV2(sourceFile, spikesFile)
+% Spike detection callback for silicon probes.
+% AE 2011-10-26
+
+
+channels = 1 : 32;
+
+% Extract the common reference - mean across channels at each timepoint
+extract_common_referenceSP(sourceFile);
+
+matlabpool
+
+parfor i = channels
+    fprintf('Extracting spikes from channel %d\n', i);
+    outFile = sprintf(strrep(spikesFile, '\', '\\'), i);
+    detectSpikesSPV2(sourceFile, i, outFile);
+end
+
+matlabpool close
+artifacts = cell(1, numel(channels));
