12614ea56a5ff28cb88841c9e8cc10f84ab572a7
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2015-09-02 16:08:27 +0200

    Cleaned up sort.Experimenter

--- schemas/+sort/Experimenter.m
+++ schemas/+sort/Experimenter.m
@@ -1,16 +1,10 @@
 %{
-sort.Experimenter (manual) # my newest table
-experimenter_id   : int unsigned # unique identifier for experimenter
+sort.Experimenter (lookup) # List of experimenters doing spike sorting
+experimenter_id         : int unsigned  # unique identifier for experimenter
 ---
-experimenter_name : varchar(255) # name of the experimenter
-experimenter_email = "": varchar(255) # email of the experimenter
+experimenter_name       : varchar(255)  # name of the experimenter
+experimenter_email = "" : varchar(255)  # email of the experimenter
 %}
 
 classdef Experimenter < dj.Relvar
-    methods
-        function self = Experimenter(varargin)
-            self.restrict(varargin{:});
-        end
-    end
 end
-
