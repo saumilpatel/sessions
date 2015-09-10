ffcccbc632af8202bf3c930615284549ca8ccbfc
George Denfield <george.denfield@gmail.com>
2015-09-10 11:29:46 -0500

    updated commonref handling

--- processing/spikesMultiChannelProbes.m
+++ processing/spikesMultiChannelProbes.m
@@ -2,6 +2,9 @@ function [electrodes, artifacts] = spikesMultiChannelProbes(sourceFile, spikesFi
 % Spike detection callback for multi-channel probes
 % AE 2015-07-01
 
+% Extract common reference first
+extract_common_reference(sourceFile, 'channels', unique(channels));
+
 n = size(channels, 1);
 artifacts = cell(1, n);
 if exist('parpool', 'file')
@@ -20,7 +23,14 @@ electrodes = 1 : n;
 function artifacts = run(spikesFile, sourceFile, electrode, channels)
 
 fprintf('Extracting spikes on channel group %d\n', electrode);
+
+% Get raw channel and reference data
 channels = arrayfun(@(c) sprintf('s1c%d', c), channels, 'uni', false);
-br = baseReader(sourceFile, channels);
+raw = baseReader(sourceFile, channels);
+refFile = fullfile(fileparts(sourceFile),'ref%d.h5');
+ref = baseReader(refFile);
+
+% create referenced reader and call detection method
+br = baseReaderReferenced(raw, ref);
 outFile = sprintf(strrep(spikesFile, '\', '\\'), electrode);
-artifacts = detectSpikesTetrodesV2(br, outFile);
+artifacts = detectSpikesMultiChannelProbes(br, outFile);
