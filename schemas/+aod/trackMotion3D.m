452df846f93fb01d03b846eceeef325a65e5746c
unknown <svc_atlab@AT-SSP.ad.bcm.edu>
2017-02-21 10:25:13 -0600

    1. changed variables names 2. replaced downsampling inner loop with vector operation 3. removed commented code 4. matlab style changes Nothing done here affects functionality

--- schemas/+aod/trackMotion3D.m
+++ schemas/+aod/trackMotion3D.m
@@ -5,87 +5,82 @@ function [x,y,z,t] = trackMotion3D(fn, analysischan)
 % requires z:\libraries\matlab in path
 run(getLocalPath('/lab/libraries/hdf5matlab/setPath.m')) ;
 
-analyzenthframe = 1 ; % skip frames if > 1
-numframestoaverageforref = 10 ; % frames to average to determine the reference frame against which motion is computed
-usemulticore = true ;
-postrefstartframetime = 0 ; % N secs to skip before analyzing motion signals, in many cases, right after starting the point scan, there is a slight drift
+downsampleFactor = 1 ; % skip frames if > 1
+numFramesToAverageForRef = 10 ; % frames to average to determine the reference frame against which motion is computed
+useMultiCore = true ;
+postRefStartFrameTime = 0 ; % N secs to skip before analyzing motion signals, in many cases, right after starting the point scan, there is a slight drift
 
 br = aodReader(fn, 'Motion') ;
 br1 = AodMotionCorrectionReader(fn) ; % if motion was corrected by using aod offsets, this is where they would be read
 
 dat = br(:,:,:) ;
-numsamples = br1.sz ;
-duration = numsamples(:,1)/br1.Fs ; % sec
+numSamples = br1.sz ;
+duration = numSamples(:,1)/br1.Fs ; % sec
 
-%numframes = size(dat,1) ;
-%numchans = size(dat,3) ;
-% motion_fs = br.Fs ;
 motion_fs = size(dat,1)/duration ; % this is based on the temporal sampling rate
-delta_t = analyzenthframe/motion_fs ;
+delta_t = downsampleFactor/motion_fs ;
 
-numplanes = br.planes ;
+numPlanes = br.planes ;
 coordinates = br.motionCoordinates ;
 
 % get pmt chan data
 dat2 = squeeze(dat(:,:,analysischan)) ;
 clearvars dat ;
-gridsize = sqrt(size(dat2,2)/numplanes) ;
+gridSize = sqrt(size(dat2,2)/numPlanes) ;
 
 % frame number to start from
-postrefstartframe = max(round(postrefstartframetime*motion_fs),1) ;
+postRefStartFrame = max(round(postRefStartFrameTime*motion_fs),1) ;
 
 % chop off the first N secs
-dat2 = dat2(postrefstartframe:end, :) ;
+dat2 = dat2(postRefStartFrame:end, :) ;
 
-needreference = true ; % first get a reference frame
+needReference = true ; % first get a reference frame
 refparams = [] ;
 calibparams = [0 1; 0 1; 0 1] ; % dont apply any calibration
 shifts = zeros(size(dat2,1),3); % store motion vectors here
-dat2(1,:) = mean(dat2(1:numframestoaverageforref,:)) ; % average frames for computing reference
-[shifts(1,:), refparams] = computeshifts(dat2(1,:)',coordinates,gridsize,calibparams,refparams,needreference) ; % this position of the fitted sphere is treated as the reference
+dat2(1,:) = mean(dat2(1:numFramesToAverageForRef,:)) ; % average frames for computing reference
+[shifts(1,:), refparams] = computeshifts(dat2(1,:)',coordinates,gridSize,calibparams,refparams,needReference) ; % this position of the fitted sphere is treated as the reference
 
 % setup for parallel processing
-if (usemulticore)
+if (useMultiCore)
     try
         mypool = parpool ; % use max processors
     catch
         fprintf('Unable to start parallel pool, single processor will be used') ;
-        usemulticore = false ;
+        useMultiCore = false ;
     end
 end 
 
 % take into account frames to skip, i.e. sampling rate of motion,
 % downsample with averaging
 adat = zeros(size(dat2)) ;
-if (analyzenthframe>1)
+if (downsampleFactor>1)
     count = 0 ;
-    for ii=numframestoaverageforref+analyzenthframe:analyzenthframe:size(dat2,1)
+    for ii=numFramesToAverageForRef+downsampleFactor:downsampleFactor:size(dat2,1)
         count = count + 1 ;
-        for jj=ii-analyzenthframe+1:ii
-            adat(count,:) = adat(count,:) + dat2(jj,:) ;
-        end 
-        adat(count,:) = adat(count,:) / analyzenthframe ;
+        adat(count,:) = adat(count,:) + sum(dat2(ii-downsampleFactor+1:ii,:)) ;
+        adat(count,:) = adat(count,:) / downsampleFactor ;
     end
 else
-    adat = dat2(numframestoaverageforref+1:end,:) ;
+    adat = dat2(numFramesToAverageForRef+1:end,:) ;
     count = size(adat,1);
 end
 
-needreference=false ; % no need for a reference, start computing positions relative to the reference of spheres in subsequent motion frames
-if (usemulticore)
+needReference=false ; % no need for a reference, start computing positions relative to the reference of spheres in subsequent motion frames
+if (useMultiCore)
     parfor ii=1:count
-        [shifts(ii+numframestoaverageforref,:)] = computeshifts(adat(ii,:)',coordinates,gridsize,calibparams,refparams,needreference) ;  
+        shifts(ii+numFramesToAverageForRef,:) = computeshifts(adat(ii,:)',coordinates,gridSize,calibparams,refparams,needReference) ;  
     end 
     delete(mypool) ;
 else
     for ii=1:count
-        [shifts(ii+numframestoaverageforref,:)] = computeshifts(adat(ii,:)',coordinates,gridsize,calibparams,refparams,needreference) ; 
+        shifts(ii+numFramesToAverageForRef,:) = computeshifts(adat(ii,:)',coordinates,gridSize,calibparams,refparams,needReference) ; 
         if (rem(count,100)==0)
             fprintf('Processed %d frames', count) ;
         end 
     end 
 end 
-x = squeeze(shifts(numframestoaverageforref+1:end,2)) ;
-y = squeeze(shifts(numframestoaverageforref+1:end,1)) ;
-z = squeeze(shifts(numframestoaverageforref+1:end,3)) ;
-t=((0:1:length(x)-1)*delta_t)+(numframestoaverageforref/motion_fs) ; % the first motion position is after the last frame used for reference calculations
+x = squeeze(shifts(numFramesToAverageForRef+1:end,2)) ;
+y = squeeze(shifts(numFramesToAverageForRef+1:end,1)) ;
+z = squeeze(shifts(numFramesToAverageForRef+1:end,3)) ;
+t=((0:1:length(x)-1)*delta_t)+(numFramesToAverageForRef/motion_fs) ; % the first motion position is after the last frame used for reference calculations
--- startup.m
+++
@@ -1,6 +0,0 @@
-% startup.m
-setPath ;
-setenv('DJ_HOST', 'at-database.ad.bcm.edu');
-setenv('DJ_USER','spatel');
-setenv('DJ_PASS','spatel');
-addpath('Z:\libraries\TwoPhoton\AOD_Control\MotionCorrection') ;
